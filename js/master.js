c562f3a09f85ca8ac09230a6f1858a3af693cde5
mangohurricane <yzhang8@ualberta.ca>
2015-07-31 11:15:52 +0800

    evolve function added

--- js/master.js
+++ js/master.js
@@ -1,11 +1,10 @@
 var display=function(live){
    document.getElementById(live).style.display="inline-block";
-   console.log("displayed");
+ 
    
 }
 var hide=function(live){
    document.getElementById(live).style.display="none";
-   console.log("hidden");
    
 }
 
@@ -20,7 +19,7 @@ var creatWorld=function(){
 	var num = Math.random()*36;
 	num = parseInt(num, 10);
 	console.log(num);
-	for (var i=0; i<num; i++){
+	for (var i=0; i<=num; i++){
   			display("r"+parseInt(Math.random()*6+2, 10).toString()+"c"+parseInt(Math.random()*6+2, 10).toString());
   		}
 }
@@ -34,34 +33,56 @@ var calculateN=function(r,c){
 				if (pos==("r"+r.toString()+"c"+c.toString())){
 					count--;
 				}
-				console.log(count);
+				
 			}
 			else{
 
 			}
 		}
 	}
-        
+    console.log(count);    
     return count
 }
 
-var evolve=function(){
-		
-		for(var i=2;i<=6;i++){
-			for(var m=2;m<=6;m++){
-				console.log(i,m);
+var gameStart=function(){
+	
+		for(var i=2;i<=7;i++){
+			for(var m=2;m<=7;m++){
+				var pos ="r"+i.toString()+"c"+m.toString();
+				if (document.getElementById(pos).style.display=="none" && calculateN(i,m)==3){
+					document.getElementById(pos).style.color="black";
+
+				}
+				else if(document.getElementById(pos).style.display!="none" && (calculateN(i,m)==3 || calculateN(i,m)==2) ){
+					document.getElementById(pos).style.color="black";
+
+				}
 			}
 		}
+		
+		for(var i=2;i<8;i++){
+			for(var m=2;m<8;m++){
+				hide("r"+i.toString()+"c"+m.toString());
+			}
 
+		}
 
-	
+		for(var i=2;i<8;i++){
+			for(var m=2;m<8;m++){
+				var pos ="r"+i.toString()+"c"+m.toString();
+				if(document.getElementById(pos).style.color=="black"){
+					display(pos);
+				}
+
+			}
+		}	
+		for(var i=2;i<8;i++){
+			for(var m=2;m<8;m++){
+				document.getElementById("r"+i.toString()+"c"+m.toString()).style.color="white";
+			}
+
+		}
 
 }
 
 
-
-display("r1c1");
-hide("r1c1");
- creatWorld();
-  calculateN(3,4);
-evolve();
